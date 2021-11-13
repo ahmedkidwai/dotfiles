06130b57a144523ca2647cee5ec643bb9e9223b8
Ahmed <ahmedkidwai@gmail.com>
2021-11-12 23:36:11 -0600

    Initial Commit

---
+++ ubuntu/appInstallScript.sh
@@ -0,0 +1,47 @@
+# Install Curl
+echo "Installing Curl"
+sudo snap install curl
+echo "Curl Installed"
+
+# Install Edge
+echo "Installing Edge"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo apt update && sudo apt install microsoft-edge-dev
+echo "Microsoft Edge Installed"
+
+# Install Docker
+echo "Installing Docker"
+sudo snap install docker
+echo "Docker Installed"
+
+# Install Code
+echo "Installing Code"
+sudo snap install code --classic
+echo "Code Installed"
+
+# Install PHPStorm
+echo "Installing PHPStorm"
+sudo snap install phpstorm --classic
+echo "PHPStorm Installed"
+
+# Install Goland
+echo "Installing PHPStorm"
+sudo snap install goland --classic
+echo "Goland Installed"
+
+# Install Spotify
+echo "Installing Spotify"
+sudo snap install spotify
+echo "Spotify Installed"
+
+
+
+
+
+
+
+
+
+
